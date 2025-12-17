b3d7abdfa52bbdc4d057864a1fea96dd41d49070
khanh <123253526+Hello2k2@users.noreply.github.com>
2025-12-17 17:45:55 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -193,7 +193,7 @@ function Load-Module ($ScriptName) {
 
 # --- 6. GUI CONSTRUCTION ---
 $Form = New-Object System.Windows.Forms.Form
-$Form.Text = "PHAT TAN PC TOOLKIT V11.8 (AUTO FIX TEMP)"
+$Form.Text = "PHAT TAN PC TOOLKIT V11.8 "
 $Form.Size = New-Object System.Drawing.Size(1050, 750)
 $Form.StartPosition = "CenterScreen"
 $Form.FormBorderStyle = "FixedSingle"; $Form.MaximizeBox = $false; $Form.Opacity = 0
