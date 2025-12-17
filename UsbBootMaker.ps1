f0ab0d855dea3f028e553e7f210f815b68d002be
khanh <123253526+Hello2k2@users.noreply.github.com>
2025-12-17 22:03:57 +0700

    Update UsbBootMaker.ps1

--- UsbBootMaker.ps1
+++ UsbBootMaker.ps1
@@ -75,7 +75,7 @@ function Get-DriveLetterByLabel ($Label) {
 
 # --- GUI INIT (RESPONSIVE LAYOUT) ---
 $Form = New-Object System.Windows.Forms.Form
-$Form.Text = "USB BOOT MAKER PRO - GLIM EDITION"
+$Form.Text = "USB BOOT MAKER PRO v1.0 - GLIM EDITION"
 $Form.Size = New-Object System.Drawing.Size(800, 650)
 $Form.StartPosition = "CenterScreen"
 $Form.BackColor = $Theme.BgForm
